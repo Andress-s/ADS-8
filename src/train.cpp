d1f15f523f54e4d59fa00460572ec6295b1ee7e0
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-05-12 11:35:10 +0300

    b55

--- src/train.cpp
+++ src/train.cpp
@@ -3,57 +3,46 @@
 #include <iostream>
 using namespace std;
 
-class Train {
-private:
-    struct Cage {
-        bool light; // состояние лампочки
-        Cage* next;
-        Cage* prev;
-    };
-    int countOp; // счетчик шагов (число переходов из вагона в вагон)
-    Cage* first; // точка входа в поезд (первый вагон)
-public:
-    Train() {
+Train::Train() {
         first = nullptr;
-        countOp = 0;
+        countOp = 1;
+}
+
+void Train::addCage(bool light) {
+    if (first == nullptr) {
+        first = new Cage();
+        first->light = light;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Cage* newCage = new Cage();
+        newCage->light = light;
+        newCage->prev = first->prev;
+        newCage->next = first;
+        first->prev->next = newCage;
+        first->prev = newCage;
     }
-    void addCage(bool light) {
-        if (first == nullptr) {
-            first = new Cage();
-            first->light = light;
-            first->next = first;
-            first->prev = first;
-        }
-        else {
-            Cage* newCage = new Cage();
-            newCage->light = light;
-            newCage->prev = first->prev;
-            newCage->next = first;
-            first->prev->next = newCage;
-            first->prev = newCage;
-        }
+}
+int Train::getLength() {
+    if (first == nullptr) {
+        return 0;
     }
-    int getLength() {
-        if (first == nullptr) {
-            return 0;
-        }
-        int length = 1;
-        Cage* current = first->next;
-        while (current != first) {
-            length++;
-            current = current->next;
-        }
-        return length;
+    int length = 1;
+    Cage* current = first->next;
+    while (current != first) {
+        length++;
+        current = current->next;
     }
-    int getOpCount() {
-        return countOp;
-    }
-    void moveNext() {
-        first = first->next;
-        countOp++;
-    }
-    void movePrev() {
-        first = first->prev;
-        countOp++;
-    }
-};
+    return length;
+}
+int Train::getOpCount() {
+    return countOp;
+}
+void Train::moveNext() {
+    first = first->next;
+    countOp++;
+}
+void Train::movePrev() {
+    first = first->prev;
+    countOp++;
+}
