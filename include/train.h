f403bc1377dbca9848e1a28d2150b7f2076b01c4
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-05-12 11:26:19 +0300

    b55

--- include/train.h
+++ include/train.h
@@ -4,56 +4,17 @@
 
 class Train {
  private:
-    struct Cage {
-        bool light; // состояние лампочки
-        Cage* next;
-        Cage* prev;
-    };
-    int countOp; // счетчик шагов (число переходов из вагона в вагон)
-    Cage* first; // точка входа в поезд (первый вагон)
+  struct Cage {
+    bool light; // состояние лампочки
+    Cage *next;
+    Cage *prev;
+  };
+  int countOp; // счетчик шагов (число переходов из вагона в вагон)
+  Cage *first, *last; // точка входа в поезд (первый вагон)
  public:
-    Train() {
-        first = nullptr;
-        countOp = 0;
-    }
-    void addCage(bool light) {
-        if (first == nullptr) {
-            first = new Cage();
-            first->light = light;
-            first->next = first;
-            first->prev = first;
-        }
-        else {
-            Cage* newCage = new Cage();
-            newCage->light = light;
-            newCage->prev = first->prev;
-            newCage->next = first;
-            first->prev->next = newCage;
-            first->prev = newCage;
-        }
-    }
-    int getLength() {
-        if (first == nullptr) {
-            return 0;
-        }
-        int length = 1;
-        Cage* current = first->next;
-        while (current != first) {
-            length++;
-            current = current->next;
-        }
-        return length;
-    }
-    int getOpCount() {
-        return countOp;
-    }
-    void moveNext() {
-        first = first->next;
-        countOp++;
-    }
-    void movePrev() {
-        first = first->prev;
-        countOp++;
-    }
+  Train();
+  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
+  int getLength();          // вычислить длину поезда
+  int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
-#endif
+#endif  // INCLUDE_TRAIN_H_
